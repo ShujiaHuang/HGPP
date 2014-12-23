20ff368a95c4a1a91c67e7235ac5171eaae64eea
Shujia <huangshujia9@gmail.com>
2014-12-23 03:23:28 +0100

    delete some files

--- example/anchor_fosmid/output/step02.merge_fosmid.sh
+++
@@ -1,2 +0,0 @@
-ls /faststorage/home/siyang/USER/huangshujia/Other/Test/HGPP/example/anchor_fosmid/output/fosmid_regions/*/*.region > fosmid_regions.lst 
-perl ../../../bin/anchor/cat.pl fosmid_regions.lst > fosmid.regions.txt
