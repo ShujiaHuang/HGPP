e6e1ff0f26f72bde4555bc111b5568176b6db52b
Siyang Liu bpl435@alumni.ku.dk <siyang@fe1.genomedk.net>
2014-12-23 03:13:10 +0100

    solving the library linking problem of bamtools

---
+++ example/anchor_fosmid/output/step02.merge_fosmid.sh
@@ -0,0 +1,2 @@
+ls /faststorage/home/siyang/USER/huangshujia/Other/Test/HGPP/example/anchor_fosmid/output/fosmid_regions/*/*.region > fosmid_regions.lst 
+perl ../../../bin/anchor/cat.pl fosmid_regions.lst > fosmid.regions.txt
